cf867bf54806354991e7ee4c9c9fb54af536d437
zf <67127582+zfofficial@users.noreply.github.com>
2021-09-15 22:12:14 +0200

    Add files via upload

--- cookie autoclicker.py
+++ cookie autoclicker.py
@@ -30,7 +30,7 @@ print("""
 """)
 
 while True:
-    if "cookies - Cookie Clicker" in GetWindowText(GetForegroundWindow()):
+    if "cookies - Cookie Clicker" in GetWindowText(GetForegroundWindow()) and "ascension" not in GetWindowText(GetForegroundWindow()).lower():
         window = GetWindowRect(GetForegroundWindow())
         wx = window[0]
         wy = window[1]
@@ -38,8 +38,8 @@ while True:
         wh = window[3] - wy
         x, y = position()
         if wx<x<wx+ww and wy<y<wy+wh:
-            centerx = wx+(ww/100)*15
-            centery= wy+(wh/100)*40
+            centerx = wx+(ww/100)*15    #THIS IS WHERE THE COOKIE's CENTER IS HORIZONTALLY  REWRITE TO wx+(ww/100)*17 if you are using opera gx (or play in fullscreen) (not tested)
+            centery = wy+(wh/100)*40    #THIS IS WHERE THE COOKIE's CENTER IS VERTICALLY    REWRITE TO wy+(wh/100)*55 if you are using a browser (or play in fullscreen)
             if centerx-128<x<centerx+128 and centery-128<y<centery+128:
                 Controller().click(Button.left)
         sleep(0.01)
